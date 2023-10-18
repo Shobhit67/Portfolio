e679f254f1f0888e79294e1a9afeed74164f1a32
Shobhit goyal <armyshobhit67@gmail.com>
2023-10-18 18:18:01 +0530

    timeline done

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,7 +1,30 @@
+import timelineElements from "./timelineElements";
+
 function Projects() {
   return (
     <div className="Projects-section">
       <h3 className="Projects-heading">Projects</h3>
+      <hr />
+
+      <div className="timeline-section">
+
+        <div className="timeline-items">
+
+          {timelineElements.map((elements) => (
+            <div className="timeline-item" key={elements.id}>
+              <div className="timeline-dot"></div>
+              <div className="timeline-date">{elements.date}</div>
+              <div className="timeline-content">
+                <h3>{elements.projectName}</h3>
+                <p className="description">
+                {elements.description}
+                </p>
+                <button className="timeline-btn" typeof="submit"><a href={elements.hrefLink}>{elements.btnTxt}</a></button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/timelineElements.js
@@ -0,0 +1,37 @@
+const timelineElements = [
+  {
+    id: 1,
+    projectName: "Portfolio",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit architecto assumenda vero magnam hic veritatis nulla minima perspiciatis ratione harum explicabo rem nostrum ad ",
+    date: "Oct 2023",
+    btnTxt: "Show Project 1",
+    hrefLink: "https://shobhitgoyal.netlify.app/"
+  },
+  {
+    id: 2,
+    projectName: "Gym",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit architecto assumenda vero magnam hic veritatis nulla minima perspiciatis ratione harum explicabo rem nostrum ad ",
+    date: "Oct 2023",
+    btnTxt: "Show Project 2",
+  },
+  {
+    id: 3,
+    projectName: "GamingZone",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit architecto assumenda vero magnam hic veritatis nulla minima perspiciatis ratione harum explicabo rem nostrum ad, aliquid eligendi deleniti? Laborum, eligendi consequatur. ",
+    date: "Oct 2023",
+    btnTxt: "Show Project 3",
+  },
+  {
+    id: 4,
+    projectName: "Pharmacy Management",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit architecto assumenda vero magnam hic veritatis nulla minima perspiciatis ratione harum explicabo rem nostrum ad, aliquid eligendi deleniti? Laborum, eligendi consequatur. ",
+    date: "Oct 2023",
+    btnTxt: "Show Project 4",
+  },
+];
+
+export default timelineElements;
