9046ab9266d8571c719403b1a1051add4ad70237
Shobhit goyal <armyshobhit67@gmail.com>
2023-10-19 14:10:38 +0530

    Change meet logo

--- src/Components/Coffee.jsx
+++ src/Components/Coffee.jsx
@@ -3,11 +3,8 @@ import CoffeeCup from "../imagex/cup6.gif";
 function Coffee() {
   return (
     <div className="meet">
-        <img className = "animated" src={CoffeeCup} alt="Buy me Coffee!" />
-        {/* &#127866;  1beercup */}
-        {/* &#127867; 2beercups */}
-        {/* &#127846; ice-cream */}
-          </div>
+        <img src={CoffeeCup} alt="Buy me Coffee!"/>
+    </div>
   );
 }
 
