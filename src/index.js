a6210e3b1e4aeb799d21f65afc668010f2f2326e
Shobhit goyal <armyshobhit67@gmail.com>
2024-02-02 14:14:29 +0530

    Stay connected done

--- src/index.js
+++ src/index.js
@@ -6,5 +6,4 @@ import App from "./Components/App";
 const root = document.getElementById('root');
 const rootInstance = createRoot(root);
 rootInstance.render(<App />);
-alert("New features coming soon. Keep exploring!");
 
